ccde7148b45e87a1f31888b6dd03146c41b7c1a2
Mansithakkar06 <mansithakkar203@gmail.com>
2024-10-18 18:31:12 +0530

    functionalities till product details

---
+++ ecomm/app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . views import *
+
+app_name = "ecomm"
+urlpatterns = [
+    path("",HomeView.as_view(),name="home"),
+    path("about/",AboutView.as_view(),name="about"),
+    path("contact_us/",ContactView.as_view(),name="contact"),
+    path("all_products/",AllProductsView.as_view(),name="allproducts"),
+    path("product/<slug:slug>/",ProductDetailView.as_view(),name="productdetail"),
+    path("add_to_cart-<int:pro_id>/",AddToCartView.as_view(),name="addtocart"),
+]
