c98494e39a94fd15f3aa7340717c01cb3221d4f1
Disha Prajapati <dishap277@gmail.com>
2024-10-20 03:47:03 +0530

    added customer login/logout functionality

--- ecomm/app/forms.py
+++ ecomm/app/forms.py
@@ -14,8 +14,20 @@ class CustomerRegistrationForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
     email = forms.CharField(widget=forms.EmailInput())
     
-   
     
     class Meta:
         model = Customer
         fields = ["username","password","email","full_name", "address"]
+    
+    
+    def clean_username(self):
+        uname = self.cleaned_data.get("username")
+        if User.objects.filter(username = uname).exists():
+            raise forms.ValidationError("Username already exists.")
+        
+        return uname
+
+
+class CustomerLoginForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput())
+    password = forms.CharField(widget=forms.PasswordInput())
